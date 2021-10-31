8ddf7e8a6ce0add62fa1aef4264f17941073924b
DaEagle01 <fbinkarim01@gmail.com>
2021-10-31 09:17:05 +0600

    a small part left

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = 5000;
+// middlewire
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dngm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("allPlaces");
+    const placeCollection = database.collection("places");
+    const allBookingCollection = database.collection("allbooking");
+
+    app.get("/places", async (req, res) => {
+      const result = await placeCollection.find({}).toArray();
+      // console.log(result);
+      res.send(result);
+    });
+
+    // add places to the database
+    app.post("/places", async (req, res) => {
+      const result = await placeCollection.insertOne(req.body);
+      console.log(result);
+    });
+
+    // get single api
+    app.get("/places/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const user = await placeCollection.findOne(query);
+      res.json(user);
+    });
+
+    // insert data
+    app.post("/places/:id", async (req, res) => {
+      const result = await allBookingCollection.insertOne(req.body);
+      // console.log(result);
+    });
+
+    // all users api
+    app.get("/allusers", async (req, res) => {
+      const result = await allBookingCollection.find({}).toArray();
+      res.send(result);
+      // console.log(result);
+    });
+
+    app.get("/allusers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allBookingCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/allusers/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateStatus = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updateStatus.status,
+        },
+      };
+
+      const result = await allBookingCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // delete a users booking
+    app.delete("/allusers/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = { _id: ObjectId(id) };
+      const result = await allBookingCollection.deleteOne(query);
+      // console.log(res.body);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World! whats going on? not really good. but Alhamduillah. !");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ test.js
@@ -0,0 +1,91 @@
+const places = [
+  {
+    img: "https://i.ibb.co/SP17T4t/sea-travel-destination-e1627461323415.jpg",
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/the-lion-statue-of-bali-e1627461194931.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/london-landmark-towerbridge-e1627461203861.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/the-ancient-ruins-of-paestum-e1627461213376.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/the-university-of-zurich-e1627461225555.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/tram-in-oslo-e1627461234668.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/grizly-bear-at-alaska-e1627461242508.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/tokyo-e1627461251282.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+  {
+    img: "https://kreativ.space/thindak/wp-content/uploads/sites/13/2021/07/bali-island-landscape-e1627461273706.jpg",
+
+    name: "Avali beach, Lefkada island, Greece.",
+    detail: "Water is magnificent and it has some peaceful vibe ",
+
+    description:
+      "Very nice beach. There are lot of space, so bring all you need for a day at beach and enjoy. There are rocks in the water. The insane blue kind of water is amazing. Road to the beach is heavy, narrow, not well paved... Parking space is limited, small.. I haven't seen that there is a possibility for paid parking..Small bar is self service type. You have drinks and some limitied number of meals. Kitchen is open from 2pm to 7pm.",
+    price: 250,
+  },
+];
